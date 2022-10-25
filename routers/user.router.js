5ae6b6226bc1a3ec637f02b0c63d56d78770db0d
pasindubhasura <srmpasindubhasura@gmail.com>
2022-10-25 22:34:49 +0530

    added a route

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,7 +1,8 @@
-const { login } = require("../controllers/user.cotroller");
+const { login, register } = require("../controllers/user.cotroller");
 
 const userRouter = require("express").Router();
 
 userRouter.post("/login", login);
+userRouter.post("/register", register);
 
 module.exports = userRouter;
